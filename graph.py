84095cbc6706d55c824a6f5437c74e7879877806
snouragan <ahaiducescu@yahoo.com>
2021-08-02 18:35:19 +0300

    output to float

--- graph.py
+++ graph.py
@@ -4,13 +4,19 @@ import plotext as plt
 import numpy as np
 import subprocess as sub
 
-
 bash = ["/opt/vc/bin/vcgencmd", "measure_temp"]
 
 process = sub.Popen(bash, stdout=sub.PIPE)
-
 output, error = process.communicate()
 
-print(output)
+temp = output[5:9]
+temp = temp.decode()
+ftemp = float(temp)
+
+#print(output)
+print(ftemp)
+
+#marker = dot
+#color = artic / teal / basil
 
 exit()
