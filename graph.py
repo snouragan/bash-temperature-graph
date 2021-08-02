e465cb68e582dea6a07ceda8f255f0d1d66ec4f8
snouragan <ahaiducescu@yahoo.com>
2021-08-03 00:05:03 +0300

    methods

--- graph.py
+++ graph.py
@@ -6,15 +6,30 @@ import subprocess as sub
 
 bash = ["/opt/vc/bin/vcgencmd", "measure_temp"]
 
-process = sub.Popen(bash, stdout=sub.PIPE)
-output, error = process.communicate()
 
-temp = output[5:9]
-temp = temp.decode()
-ftemp = float(temp)
+def getTemp():
+    process = sub.Popen(bash, stdout=sub.PIPE)
+    output, error = process.communicate()
+    return output
+
+def decodeTemp(output):
+    temp = output[5:9]
+    temp = temp.decode()
+    ftemp = float(temp)
+    return ftemp
 
 #print(output)
-print(ftemp)
+#print(ftemp)
+
+tempList = []
+
+for x in range(5):
+    tempList.append(decodeTemp(getTemp()))
+
+plt.scatter(tempList)
+plt.plotsize(50,30)
+plt.title("Temperatura")
+plt.show()
 
 #marker = dot
 #color = artic / teal / basil
