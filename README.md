9a1d8f50b9b1c212a18436d06ad78dc4c1f94f1d
snouragan <ahaiducescu@yahoo.com>
2021-08-03 15:38:12 +0300

    working as intended

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# bash-temperature-graph
+bash-temperature-graph
+
+TO DO:
+
+ +update list one by one to a fixed number of values without  clearing the graph
+ +erase first element, add last element, clear, show
+
+
--- graph.py
+++ graph.py
@@ -5,6 +5,9 @@ import numpy as np
 import subprocess as sub
 
 bash = ["/opt/vc/bin/vcgencmd", "measure_temp"]
+frames = 20
+l = 1000
+dots = 10
 
 def getTemp():
     process = sub.Popen(bash, stdout=sub.PIPE)
@@ -20,35 +23,53 @@ def decodeTemp(output):
 def getDecodedTemp():
     return decodeTemp(getTemp())
 
-
-l, n = 1000, 2
-x = np.arange(0, l)
-xticks = np.linspace(0, l - 1, 5)
-xlabels = [str(i) + "?" for i in range(5)]
-frames = 20
+def setup():
+    global x
+    x = np.arange(1, l)
+    xticks = np.linspace(1, l-1, 10)
     
-plt.clf()
-plt.ylim(50, 70)
-plt.xticks(xticks, xlabels)
-plt.yticks([50, 60, 70])
-plt.plotsize(100, 30)
-plt.title("Streaming Data")
-plt.colorless()
+    xlabels = [str(i) for i in range(10)]
 
-while True:
-    tempList = []
 
-    for j in range(10):
+    plt.clf()
+    plt.ylim(50, 70)
+    plt.xticks(xticks, xlabels)
+    plt.yticks([50, 60, 70])
+    plt.plotsize(100, 30)
+    plt.title("Temperature")
+    plt.colorless()
+
+def fillTemp(tempList):
+    length = len(tempList)
+    if length < dots:
+        for i in range(10):
+            tempList.append(getDecodedTemp())
+
+    elif length == dots:
+        tempList.pop(0)
         tempList.append(getDecodedTemp())
 
+    return tempList
 
-    y = tempList
+def plot(y):
+    plt.cld()
+    plt.clt()
+    plt.scatter(x, y, marker = "dot")
+    plt.sleep(0.05)
+    plt.show()
 
-    for i in range(frames):
+
+def main():
+    
+    setup()
+
+    tempList =[]
+
+    while True:
+        tempList = fillTemp(tempList)
+        y = tempList
         
-        plt.clp
-        plt.cld()
-        plt.clt()
-        plt.scatter(x, y, marker = "dot")
-        plt.sleep(0.05)
-        plt.show()
+        #for i in range(frames):
+        plot(y)
+
+main()
