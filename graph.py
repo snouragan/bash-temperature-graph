b2fe2d50a68ecd7f9f245c4497f14fb59ed1ec84
snouragan <ahaiducescu@yahoo.com>
2021-08-03 16:12:25 +0300

    adjustments

--- graph.py
+++ graph.py
@@ -5,9 +5,9 @@ import numpy as np
 import subprocess as sub
 
 bash = ["/opt/vc/bin/vcgencmd", "measure_temp"]
-frames = 20
+frames = 2  
 l = 1000
-dots = 10
+dots = 10 
 
 def getTemp():
     process = sub.Popen(bash, stdout=sub.PIPE)
@@ -26,15 +26,15 @@ def getDecodedTemp():
 def setup():
     global x
     x = np.arange(1, l)
-    xticks = np.linspace(1, l-1, 10)
+    xticks = np.linspace(1, l-1, dots)
     
-    xlabels = [str(i) for i in range(10)]
+    xlabels = [str(i) for i in range(dots)]
 
 
     plt.clf()
     plt.ylim(50, 70)
     plt.xticks(xticks, xlabels)
-    plt.yticks([50, 60, 70])
+    plt.yticks([50, 55, 60, 65, 70])
     plt.plotsize(100, 30)
     plt.title("Temperature")
     plt.colorless()
@@ -42,7 +42,7 @@ def setup():
 def fillTemp(tempList):
     length = len(tempList)
     if length < dots:
-        for i in range(10):
+        for i in range(dots):
             tempList.append(getDecodedTemp())
 
     elif length == dots:
@@ -54,13 +54,13 @@ def fillTemp(tempList):
 def plot(y):
     plt.cld()
     plt.clt()
-    plt.scatter(x, y, marker = "dot")
+    plt.scatter(x, y, marker = "small", color = "artic", fillx = True)
     plt.sleep(0.05)
     plt.show()
 
 
 def main():
-    
+
     setup()
 
     tempList =[]
@@ -69,7 +69,7 @@ def main():
         tempList = fillTemp(tempList)
         y = tempList
         
-        #for i in range(frames):
-        plot(y)
+        for i in range(frames):
+            plot(y)
 
 main()
